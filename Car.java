55012ca172d9fa114b59dfd8e67bd623f47a3620
ainugiri <40005294+ainugiri@users.noreply.github.com>
2025-08-01 14:39:48 +0530

    Car Class added

---
+++ Car.java
@@ -0,0 +1,30 @@
+class Car extends Vehicle {
+    private String model;
+    private int year;
+
+    public Car(String model, int year) {
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car("Toyota Camry", 2020);
+        System.out.println(car);
+    }
+}
