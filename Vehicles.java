e9c00447f6ef50433057ea44e166b3bb6b74c7ae
ainugiri <40005294+ainugiri@users.noreply.github.com>
2025-08-01 14:31:34 +0530

    vehicles class added

---
+++ Vehicles.java
@@ -0,0 +1,44 @@
+class Vehicles {
+    private String vehicleType;
+    private String vehicleModel;
+    private int vehicleYear;
+
+    public Vehicles(String vehicleType, String vehicleModel, int vehicleYear) {
+        this.vehicleType = vehicleType;
+        this.vehicleModel = vehicleModel;
+        this.vehicleYear = vehicleYear;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getVehicleModel() {
+        return vehicleModel;
+    }
+
+    public void setVehicleModel(String vehicleModel) {
+        this.vehicleModel = vehicleModel;
+    }
+
+    public int getVehicleYear() {
+        return vehicleYear;
+    }
+
+    public void setVehicleYear(int vehicleYear) {
+        this.vehicleYear = vehicleYear;
+    }
+    public void displayVehicleInfo() {
+        System.out.println("Vehicle Type: " + vehicleType);
+        System.out.println("Vehicle Model: " + vehicleModel);
+        System.out.println("Vehicle Year: " + vehicleYear);
+    }   
+    public static void main(String[] args) {
+        Vehicles car = new Vehicles("Car", "Ford Freestyle", 2021);
+        car.displayVehicleInfo();
+    }
+}
